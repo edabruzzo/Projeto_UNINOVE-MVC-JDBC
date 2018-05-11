49be954969eac91a7e91a41eebe18783ba6a4f00
edabruzzo <emmanuel.oliveira3@gmail.com>
2018-05-11 08:43:06 -0300

    Alterações no Servlet CriaInfra

--- src/java/Controller/CriaInfraServletController.java
+++ src/java/Controller/CriaInfraServletController.java
@@ -8,8 +8,6 @@ package Controller;
 import DAO.UsuarioDAO;
 import Util.OperacoesBancoDados;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -30,50 +28,8 @@ public class CriaInfraServletController extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     OperacoesBancoDados fabrica = new OperacoesBancoDados();
+
     UsuarioDAO usuarioDAO = new UsuarioDAO();
-
-    
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-
-
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet CriaInfraServletController</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet CriaInfraServletController at " + request.getContextPath() + "</h1>");
-            
-            
-            out.println("</body>");
-            out.println("</html>");
-            
-            try {
-            out.println("Criando a base de dados...");
-            criarBaseDados(request, response);
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(HomeServletController.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (SQLException ex) {
-            Logger.getLogger(HomeServletController.class.getName()).log(Level.SEVERE, null, ex);
-            out.println("Verifique se a base já existe ...");
-
-        }
-
-        }
-    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
@@ -87,9 +43,8 @@ public class CriaInfraServletController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
-    }
 
+    }
 
     /**
      * Returns a short description of the servlet.
@@ -101,73 +56,39 @@ public class CriaInfraServletController extends HttpServlet {
         return "Short description";
     }// </editor-fold>
 
-    
-    
-        // When the usuario enters usuarioName & password, and click Submit.
+    // When the usuario enters usuarioName & password, and click Submit.
     // This method will be executed.
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
-        try {
-            criarBaseDados(request, response);
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(CriaInfraServletController.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (SQLException ex) {
-            Logger.getLogger(CriaInfraServletController.class.getName()).log(Level.SEVERE, null, ex);
-        }
 
-
-    }
-
-    public void criarBaseDados(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException, ClassNotFoundException, SQLException {
-
-        String errorString = null;
-        Connection conn = null;
+        String errorString = "";
 
         try {
+            fabrica.criaBaseDados();
+            fabrica.criaInfraestrutura();
 
-            usuarioDAO.findUsuario(1);
-            errorString = "A base de dados não foi criada com sucesso!";
+            errorString = "A base de dados foi criada com sucesso!";
             request.setAttribute("errorString", errorString);
             // Forward to /WEB-INF/views/login.jsp
             RequestDispatcher dispatcher //
                     = this.getServletContext().getRequestDispatcher("WEB-INF/view/loginView.jsp");
             dispatcher.forward(request, response);
 
-        } catch (Exception e) {
-            e.getMessage();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(CriaInfraServletController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(CriaInfraServletController.class.getName()).log(Level.SEVERE, null, ex);
 
-            fabrica.criaBaseDados();
-
-            try {
-
-                fabrica.criaInfraestrutura();
-                errorString = "A infraestrutura foi criada com sucesso!";
-                request.setAttribute("errorString", errorString);
-                // Forward to /WEB-INF/views/login.jsp
-                RequestDispatcher dispatcher //
-                        = this.getServletContext().getRequestDispatcher("/WEB-INF/views/loginView.jsp");
-                dispatcher.forward(request, response);
-
-            }catch (IOException | ClassNotFoundException | SQLException | ServletException multiException) {
-                e.printStackTrace();
-                errorString = "A infraestrutura não foi criada!";
-                request.setAttribute("errorString", errorString);
-                // Forward to /WEB-INF/views/login.jsp
-                RequestDispatcher dispatcher //
-                        = this.getServletContext().getRequestDispatcher("/WEB-INF/views/loginView.jsp");
-                dispatcher.forward(request, response);
-
-            } finally {
-
-                fabrica.fecharConexao(conn);
-
-            }
+            errorString = "A base de dados NÃO foi criada!";
+            request.setAttribute("errorString", errorString);
+            // Forward to /WEB-INF/views/login.jsp
+            RequestDispatcher dispatcher //
+                    = this.getServletContext().getRequestDispatcher("homeView.jsp");
+            dispatcher.forward(request, response);
 
         }
-    }
-}
 
-    
+    }
+
+}
